24c376dbafa5fe05f305b2d6467d051ccfaa6d84
josue <josue@git.com>
2019-12-26 20:30:32 +0100

    registro y login configurado

--- app/User.php
+++ app/User.php
@@ -16,7 +16,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'surname', 'role', 'nick', 'email', 'password', 'image'
+        'role', 'name', 'surname', 'nick', 'email', 'password', 'image'
     ];
 
     /**
--- routes/web.php
+++ routes/web.php
@@ -11,12 +11,12 @@
 |
 */
 
-Route::get('/', function () {
+/* Route::get('/', function () {
     return view('welcome');
-})->name('welcome');
+})->name('welcome'); */
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/', 'HomeController@index')->name('home');
 
 Auth::routes();
